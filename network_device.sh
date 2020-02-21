050b778f1aec2deb8e06c60db891294cb25830f9
Qiaolun Zhang <qiaolunzhang@gmail.com>
2020-02-21 17:11:40 +0100

    add changing network device

---
+++ network_device.sh
@@ -0,0 +1,18 @@
+# consumer-> router1 -> router ->publisher
+
+# visualize
+ifconfig enp0s25:0 192.168.80.133
+# publisher
+ifconfig enp0s25:1 192.168.80.134
+# router
+ifconfig enp0s25:2 192.168.80.135
+# router1
+ifconfig enp0s25:3 192.168.80.136
+# consumer
+ifconfig enp0s25:4 192.168.80.137
+# firewall
+ifconfig enp0s25:5 192.168.80.138
+# client1
+ifconfig enp0s25:6 192.168.80.139
+# client2
+ifconfig enp0s25:7 192.168.80.140
