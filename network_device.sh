f1404fd6c2364de08e2f0ff875cb7a011367b5a2
Qiaolun Zhang <qiaolunzhang@gmail.com>
2020-02-21 22:02:44 +0100

    correct the ip address

--- network_device.sh
+++ network_device.sh
@@ -1,3 +1,3 @@
-ifconfig ens33:0 192.168.80.2
-ifconfig ens33:1 192.168.80.3
-ifconfig ens33:2 192.168.80.4
+ifconfig ens33:0 192.168.221.2
+ifconfig ens33:1 192.168.221.3
+ifconfig ens33:2 192.168.221.4
